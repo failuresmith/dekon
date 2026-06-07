aca67fd0b5ef8c9a4c8e05e64852069058149cfd
FS <failuresmith@proton.me>
2026-06-07 09:50:05 +0330

    Bump version to 0.2.0

--- lib/src/app_config.dart
+++ lib/src/app_config.dart
@@ -2,7 +2,7 @@ class AppConfig {
   const AppConfig._();
 
   static const appName = 'Dekon';
-  static const appVersion = '0.1.4+5';
+  static const appVersion = '0.2.0+6';
   static const applicationId = 'xyz.infinica.dekon';
   static const minAndroidSdk = 24;
   static const githubReleasesUpdateEnabled = false;
